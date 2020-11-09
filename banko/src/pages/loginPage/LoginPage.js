a6ee9bcc5f7c20c8ccceb045b3a82b4de8ab19da
irock194 <gabrieltenocelotl@gmail.com>
2020-11-09 11:35:26 -0800

    test login page

---
+++ banko/src/pages/loginPage/LoginPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Navbar from '../../components/Navbar/Navbar'
+import PageContent from './components/PageContent';
+import Footer from '../../components/Footer/Footer'
+
+function LoginPage()
+{
+    return (
+        <div>
+            <Navbar/>
+            <PageContent/>
+            <Footer/>
+        </div>
+    )
+}
---
+++ banko/src/pages/loginPage/components/LoginContainer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import styles from './LoginContainer.css'
+
+function LoginContainer()
+{
+    return
+    (
+        <div className={styles.login_content}>
+            <div class={styles.inner_login_content}>
+                <div class="login_tile">
+                    <div class="login_title">
+                        <u1 class="list">
+                            <li>Customer Login</li>
+                            <li><input type="text" name="User Name" placeholder="User Name"></li>
+                            <li><input type="password" name="Password" placeholder="......"></li>
+                            <li><input type="button" name="Submit" value="Submit"></li>
+                        <u1>
+                    </div>
+                </div>
+                <div class="register_tile">
+                    <div class="login_tile">
+                        <div class="login_title">
+                            <u1 class="list">
+                                <li>Register</li>
+                                <li><input type="text" name="User Name" placeholder="User Name"></li>
+                                <li><input type="password" name="Password" placeholder="Password"></li>
+                                <li><input type="button" name="Submit" value="Register"></li>
+                            <u1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    )
+
+}
