d3251755ae396cba4e1cf4c4d7b6c3259af69bee
irock194 <gabrieltenocelotl@gmail.com>
2020-11-30 10:50:13 -0800

    cleaned up some code

--- banko/src/pages/registerPage/RegisterPage.js
+++ banko/src/pages/registerPage/RegisterPage.js
@@ -4,18 +4,7 @@ import Navbar from '../../components/Navbar/Navbar'
 import RegisterContainer from './components/RegisterContainer';
 import Footer from '../../components/Footer/Footer'
 
-/*
-function RegisterPage()
-{
-    return (
-        <div>
-            <Navbar/>
-            <RegisterContainer/>
-            <Footer/>
-        </div>
-    )
-}
-*/
+
 function RegisterPage()
 {
     return (
