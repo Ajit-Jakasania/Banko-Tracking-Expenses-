1d5c92271afeab82fa31c33b9ab1db94f35b751f
irock194 <gabrieltenocelotl@gmail.com>
2020-11-29 19:27:25 -0800

    frontend changes

--- banko/src/App.js
+++ banko/src/App.js
@@ -11,7 +11,7 @@ import LoginPage2 from './pages/loginPage/LoginPage2'
 
 import './App.css';
 
-
+/*
 function App() {
   return (
     <div className="App">
@@ -21,4 +21,50 @@ function App() {
   );
 }
 
+export default App;
+*/
+/*
+class App extends Component {
+
+
+
+  render(){
+    return (
+      <div className="App">
+
+        <LoginPage2 />
+      </div>
+    );
+  }
+}
+*/
+
+class App extends Component {
+ 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        totalReactPackages: null
+    };
+}
+
+componentDidMount() {
+    // Simple GET request using axios
+    axios.get('http://localhost:8080/userLogIn', {
+      username: "user1"
+    })
+    .then(function(response){ 
+      console.log(response)
+    });
+}
+
+  render() {
+      return (
+          <div className="App">
+              <LoginPage />
+          </div>
+      );
+  }
+}
 export default App;
