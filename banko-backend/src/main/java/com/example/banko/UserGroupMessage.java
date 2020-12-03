26a919f6afa821d58cc615e1cc1927cb05aa45f0
Osama Hanhan <hanhan.osama@gmail.com>
2020-12-02 19:03:11 -0800

    fixed group messages stuff

--- banko-backend/src/main/java/com/example/banko/UserGroupMessage.java
+++ banko-backend/src/main/java/com/example/banko/UserGroupMessage.java
@@ -9,21 +9,26 @@ import java.util.HashMap;
 
 public class UserGroupMessage {
 
-    public static ArrayList<HashMap<String,String>> getUserGroupMessage(int group_id) throws SQLException {
+    public static ArrayList<HashMap<String, String>> getUserGroupMessage(String group_name) throws SQLException {
         Connection connection = BankoBackendServer.connection;
-        ArrayList<HashMap<String,String>> listGroupMessages = new ArrayList<HashMap<String,String>>();
+        ArrayList<HashMap<String, String>> listGroupMessages = new ArrayList<HashMap<String, String>>();
 
+        // nested query for getting group id
+        String getGroupID = "(SELECT group_id FROM omjmf6vzmpqpgc0p.group_list WHERE group_name = '" + group_name
+                + "')";
 
-        String selectSql = "SELECT message_content_id, content,group_id, user_id, username, date_sent FROM omjmf6vzmpqpgc0p.message JOIN omjmf6vzmpqpgc0p.message_content USING (message_content_id) JOIN omjmf6vzmpqpgc0p.user USING (user_id) WHERE group_id ="+group_id+" ORDER BY date_sent DESC";
+        String selectSql = "SELECT message_content_id, content, group_id, user_id, username, date_sent FROM omjmf6vzmpqpgc0p.message "
+                + "JOIN omjmf6vzmpqpgc0p.message_content USING (message_content_id) JOIN omjmf6vzmpqpgc0p.user USING (user_id) WHERE (group_id = "
+                + getGroupID + ") ORDER BY date_sent ASC";
         Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
         ResultSet rs = statement.executeQuery(selectSql);
         while (rs.next()) {
             HashMap<String, String> userGroupMessageJSON = new HashMap<String, String>();
-            userGroupMessageJSON.put("message_content_id",Integer.toString(rs.getInt("message_content_id")));
-            userGroupMessageJSON.put("content",rs.getString("content"));
-            userGroupMessageJSON.put("group_id",Integer.toString(rs.getInt("group_id")));
-            userGroupMessageJSON.put("user_id",Integer.toString(rs.getInt("user_id")));
-            userGroupMessageJSON.put("username",rs.getString("username"));
+            userGroupMessageJSON.put("message_content_id", Integer.toString(rs.getInt("message_content_id")));
+            userGroupMessageJSON.put("content", rs.getString("content"));
+            userGroupMessageJSON.put("group_id", Integer.toString(rs.getInt("group_id")));
+            userGroupMessageJSON.put("user_id", Integer.toString(rs.getInt("user_id")));
+            userGroupMessageJSON.put("username", rs.getString("username"));
             userGroupMessageJSON.put("date_sent", (rs.getString("date_sent")));
             listGroupMessages.add(userGroupMessageJSON);
         }
