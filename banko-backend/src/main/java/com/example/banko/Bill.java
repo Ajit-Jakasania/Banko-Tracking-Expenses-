0c724ebf48665f921543623be306595b477dff5c
Ajit Jakasania <ajit.jakasania@gmail.com>
2020-12-08 18:05:11 -0800

    Fixed addBill

--- banko-backend/src/main/java/com/example/banko/Bill.java
+++ banko-backend/src/main/java/com/example/banko/Bill.java
@@ -3,11 +3,15 @@ package com.example.banko;
 import java.sql.*;
 
 public class Bill {
+    public String transaction_content;
+    public double amount;
+    public String group_name;
     private int transaction_id;
     private String photo_url;
+    private int group_id;
 
-    public Bill(int transaction_id, String photo_url) {
-        this.transaction_id = transaction_id;
+    public Bill(String group_name, String photo_url) {
+        this.group_name = group_name;
         this.photo_url = photo_url;
     }
 
@@ -19,6 +23,9 @@ public class Bill {
      */
     public int createBill() throws SQLException {
         Connection connection = BankoBackendServer.connection;
+        group_id = getGroupId(group_name, connection);
+        transaction_id = getTransactionId(group_id,connection);
+
 
         String query = "INSERT INTO omjmf6vzmpqpgc0p.bill (transaction_id, photo_url) VALUES (" + transaction_id + ", '"
                 + photo_url + "')";
@@ -37,4 +44,42 @@ public class Bill {
         return -1;
     }
 
+    private int getGroupId(String group_name, Connection connection) {
+        int group_id = -1;
+
+        String query = "SELECT group_id FROM omjmf6vzmpqpgc0p.group_list WHERE group_name = '" + group_name + "'";
+        Statement statement;
+
+        try {
+            statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+            ResultSet rs = statement.executeQuery(query);
+            rs.next();
+
+            group_id = rs.getInt("group_id");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return group_id;
+    }
+
+    private int getTransactionId(int group_id, Connection connection) {
+        int transaction_id = -1;
+
+        String query = "SELECT transaction_id FROM omjmf6vzmpqpgc0p.transaction WHERE group_id =" + group_id ;
+
+        try {
+            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+            ResultSet rs = statement.executeQuery(query);
+            rs.last();
+
+            transaction_id = rs.getInt("transaction_id");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return transaction_id;
+    }
 }
