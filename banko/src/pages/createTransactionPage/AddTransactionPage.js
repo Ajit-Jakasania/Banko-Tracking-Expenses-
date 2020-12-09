d4880e3f1cae19f6a39d37e5118dc19b737819f7
irock194 <gabrieltenocelotl@gmail.com>
2020-12-08 18:03:42 -0800

    Create New Transaction Page

---
+++ banko/src/pages/createTransactionPage/AddTransactionPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import TransactionPageContainer from './components/LoginContainer';
+
+
+function AddTransactionPage()
+{
+    return (
+        <div>
+            <TransactionPageContainer />
+        </div>
+    )
+}
+
+export default AddTransactionPage;
